dbf12e9a1fa4eddabc079e3ea8d902bde8927075
Filip Stojakovic <filips@europe.com>
2020-02-18 23:28:22 +0100

    bug fix, added duration of excetion

--- testing/src/main/java/Example.java
+++ testing/src/main/java/Example.java
@@ -1,5 +1,4 @@
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
@@ -9,20 +8,21 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.SparkSession;
-import scala.Char;
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 public class Example extends Application
@@ -31,12 +31,21 @@ public class Example extends Application
     private TextArea initialTextArea;
     @FXML
     private TextArea sortedTextArea;
+    @FXML
+    private Label durationLbl;
     private Service<String> backgroundTask;
+    public static final int NUMBER_OF_SLICES = 1000;
+    @FXML
+    Parent root = null;
+
     @FXML
     void startBtnClicked(ActionEvent event)
     {
-        backgroundTask= new Service<String>()
-        {   @Override
+        long startTime = System.currentTimeMillis();
+
+        backgroundTask = new Service<String>()
+        {
+            @Override
             public Task<String> createTask()
             {
                 return new Task<String>()
@@ -49,17 +58,29 @@ public class Example extends Application
                 };
             }
         };
+
         backgroundTask.setOnSucceeded(new EventHandler<WorkerStateEvent>()
         {
             @Override
             public void handle(WorkerStateEvent workerStateEvent)
             {
                 sortedTextArea.setText(backgroundTask.getValue().trim());
+                System.out.println("DONE");
+                long endTime = System.currentTimeMillis();
+                long duration = TimeUnit.MILLISECONDS.toSeconds(endTime - startTime);
+
+                if(duration==0)
+                    durationLbl.setText((endTime - startTime) + "ms");
+                else
+                    durationLbl.setText(duration + "s");
+
             }
         });
-        backgroundTask.start();
+        if(!"".equals(initialTextArea.getText().trim()))
+            backgroundTask.start();
 
     }
+
     public static void main(String[] args)
     {
         //START PROGRAM
@@ -70,37 +91,38 @@ public class Example extends Application
     {
 
         Logger.getLogger("org.apache").setLevel(Level.WARN);
-        List<String> result=null;
+        List<String> result = null;
         try
         {
 //            SparkSession conf = SparkSession.builder().appName("test").master("spark://10.10.1.14:7077").getOrCreate();
             SparkSession conf = SparkSession.builder().appName("test").master("local[*]").getOrCreate();
 
 
-            result= new ArrayList<>();
-            try (JavaSparkContext sc = new JavaSparkContext(conf.sparkContext()))
+            result = new ArrayList<>();
+            try (JavaSparkContext javaSparkContext = new JavaSparkContext(conf.sparkContext()))
             {
                 char inputChars[] = inputString.toCharArray();
                 List<String> tmp = new ArrayList<String>();
 
-                for(int i=0;i<inputChars.length;i++)
-                    if(' '!=inputChars[i])
+
+                for (int i = 0; i < inputChars.length; i++)
+                    if (' ' != inputChars[i] && '\n' != inputChars[i])
                         tmp.add(String.valueOf(inputChars[i]));
 
-
-                Dataset<String> stringDataset = conf.createDataset(tmp,Encoders.STRING());
+                JavaRDD<String> rdd = javaSparkContext.parallelize(tmp, NUMBER_OF_SLICES);
+                Dataset<String> stringDataset = conf.createDataset(rdd.collect(), Encoders.STRING());
                 Dataset<String> sortedDataset = stringDataset.sort(stringDataset.col(stringDataset.columns()[0]));
                 result = sortedDataset.collectAsList();
-    //            result.stream().forEach(System.out::println);
+                //            result.stream().forEach(System.out::println);
 
             } catch (Exception ex)
             {
                 ex.printStackTrace();
             }
-        }catch (Exception ex)
+        } catch (Exception ex)
         {
             System.err.println("Cluster is probably not running");
-//            ex.printStackTrace();
+            //            ex.printStackTrace();
         }
         return result.toString();
     }
@@ -110,15 +132,18 @@ public class Example extends Application
     public void start(Stage primaryStage)
     {
         String sceneFile = "fxml/Example.fxml";
-        Parent root = null;
+//        Parent root = null;
         URL url = null;
         try
         {
             url = getClass().getResource(sceneFile);
             root = FXMLLoader.load(url);
+            root.setStyle("fx-border-color:black");
             Scene scene = new Scene(root);
             primaryStage.setTitle("Generic Algotithm");
             primaryStage.setScene(scene);
+
+//            primaryStage.setResizable(false);
             primaryStage.show();
         } catch (Exception ex)
         {
